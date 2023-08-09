62709d1c0dc2d72a38b9c6c1dc405cee15a32ee9
Stefan Fisk <contact@stefanfisk.com>
2023-08-09 10:19:05 +0200

    refactor: make PhpReact always use HtmlSerializer

--- src/PhpReact.php
+++ src/PhpReact.php
@@ -4,55 +4,54 @@ declare(strict_types=1);
 
 namespace StefanFisk\PhpReact;
 
-use StefanFisk\PhpReact\Serialization\EchoingSerializerInterface;
+use Psr\Container\ContainerInterface;
 use StefanFisk\PhpReact\Serialization\Html\HtmlSerializer;
 use StefanFisk\PhpReact\Serialization\Html\Middleware\ClassAttributeMiddleware;
 use StefanFisk\PhpReact\Serialization\Html\Middleware\ClosureMiddleware;
+use StefanFisk\PhpReact\Serialization\Html\Middleware\HtmlAttributeValueMiddlewareInterface;
+use StefanFisk\PhpReact\Serialization\Html\Middleware\HtmlNodeValueMiddlewareInterface;
 use StefanFisk\PhpReact\Serialization\Html\Middleware\StringableMiddleware;
 use StefanFisk\PhpReact\Serialization\Html\Middleware\StyleAttributeMiddleware;
-use StefanFisk\PhpReact\Serialization\SerializerInterface;
+use StefanFisk\PhpReact\Support\Comparator;
 
-use function assert;
 use function ob_end_clean;
 use function ob_get_clean;
 use function ob_get_level;
 use function ob_start;
 
-/**
- * @psalm-api
- * @template T
- */
 class PhpReact
 {
-    /** @param SerializerInterface<T> $serializer */
+    private readonly HtmlSerializer $serializer;
+    private readonly Renderer $renderer;
+
+    /** @param array<HtmlAttributeValueMiddlewareInterface|HtmlNodeValueMiddlewareInterface> $middlewares */
     public function __construct(
-        private readonly Renderer $renderer = new Renderer(),
-        private SerializerInterface $serializer = new HtmlSerializer(middlewares: [
+        ContainerInterface $container = new Container(),
+        Comparator $comparator = new Comparator(),
+        array $middlewares = [
             new ClosureMiddleware(),
             new StringableMiddleware(),
             new ClassAttributeMiddleware(),
             new StyleAttributeMiddleware(),
-        ]),
+        ],
     ) {
+        $this->renderer = new Renderer(
+            nodeFactory: new NodeFactory(container: $container),
+            comparator: $comparator,
+        );
+        $this->serializer = new HtmlSerializer(middlewares: $middlewares);
     }
 
-    /**
-     * @return T
-     */
-    public function render(
-        Element $el,
-    ): mixed {
-        return $this->renderer->renderAndSerialize(
+    public function render(Element $el): void
+    {
+        $this->renderer->renderAndSerialize(
             el: $el,
             serializer: $this->serializer,
         );
     }
 
-    public function renderToString(
-        Element $el,
-    ): string {
-        assert($this->serializer instanceof EchoingSerializerInterface);
-
+    public function renderToString(Element $el): string
+    {
         $obLevel = ob_get_level();
 
         try {
@@ -60,7 +59,7 @@ class PhpReact
 
             $this->renderer->renderAndSerialize(
                 el: $el,
-                serializer: $this->serializer, // @phpstan-ignore-line
+                serializer: $this->serializer,
             );
 
             return (string) ob_get_clean();
