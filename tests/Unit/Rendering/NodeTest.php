dedfbcc574b8d19f5b23a99d71d52b35aff92c05
Stefan Fisk <contact@stefanfisk.com>
2023-08-19 07:54:47 +0200

    fix: move NodeTest into correct dir

--- tests/Unit/Rendering/NodeTest.php
+++ tests/Unit/Rendering/NodeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace StefanFisk\PhpReact\Tests\Unit\Nodes;
+namespace StefanFisk\PhpReact\Tests\Unit\Rendering;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
