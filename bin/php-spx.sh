1e6dc8d012a59cd749a8e93962d5c89be67a2541
Stefan Fisk <contact@stefanfisk.com>
2023-08-24 08:51:53 +0200

    feat: add all supported env variables to bin/php-spx.php

--- bin/php-spx.sh
+++ bin/php-spx.sh
@@ -7,7 +7,7 @@ TAG=stefanfisk/php-spx:latest
 docker build \
     --quiet \
     --tag $TAG \
-    - <<EOF
+    - >/dev/null <<EOF
 FROM php:8.2-apache
 
 RUN apt-get update
@@ -46,7 +46,19 @@ docker run \
     --user $(id -u ${USER}):$(id -g ${USER}) \
     --env SPX_ENABLED=1 \
     --env SPX_AUTO_START=${SPX_AUTO_START:-1} \
+    --env SPX_BUILTINS=${SPX_BUILTINS} \
+    --env SPX_DEPTH=${SPX_DEPTH} \
+    --env SPX_SAMPLING_PERIOD=${SPX_SAMPLING_PERIOD} \
+    --env SPX_METRICS=${SPX_METRICS} \
     --env SPX_REPORT=${SPX_REPORT:-full} \
+    --env SPX_FP_FOCUS=${SPX_FP_FOCUS} \
+    --env SPX_FP_INC=${SPX_FP_INC} \
+    --env SPX_FP_REL=${SPX_FP_REL} \
+    --env SPX_FP_LIMIT=${SPX_FP_LIMIT} \
+    --env SPX_FP_LIVE=${SPX_FP_LIVE} \
+    --env SPX_FP_COLOR=${SPX_FP_COLOR} \
+    --env SPX_TRACE_SAFE=${SPX_TRACE_SAFE} \
+    --env SPX_TRACE_FILE=${SPX_TRACE_FILE} \
     --volume "$(pwd)":/var/php \
     $TAG \
     "$@"
