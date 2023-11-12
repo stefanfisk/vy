ab4fd834c35478077603812cea427ee5dcc87f44
Stefan Fisk <contact@stefanfisk.com>
2023-11-12 17:51:00 +0100

    feat: add Fragment::el function

--- src/Components/Fragment.php
+++ src/Components/Fragment.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace StefanFisk\Vy\Components;
 
+use StefanFisk\Vy\Element;
+
 final class Fragment
 {
+    public static function el(): Element
+    {
+        return new Element(
+            key: null,
+            type: self::class,
+            props: [],
+        );
+    }
+
     public function render(mixed $children = null): mixed
     {
         return $children;
