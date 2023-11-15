636b275e74a19d07dcb80bd8e2b00d6d256b2b29
Stefan Fisk <contact@stefanfisk.com>
2023-11-15 20:08:41 +0100

    feat: add callback hook

---
+++ src/Hooks/CallbackHook.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StefanFisk\Vy\Hooks;
+
+use Closure;
+use StefanFisk\Vy\Rendering\Node;
+use StefanFisk\Vy\Rendering\Renderer;
+
+class CallbackHook extends Hook
+{
+    /**
+     * @param Closure(TArgs):TRet $fn
+     * @param array<mixed> $deps
+     *
+     * @return Closure(TArgs):TRet
+     *
+     * @template TArgs
+     * @template TRet
+     * @psalm-suppress MixedInferredReturnType,MixedReturnStatement
+     */
+    public static function use(Closure $fn, array $deps): Closure
+    {
+        // @phpstan-ignore-next-line
+        return static::useWith($fn, $deps);
+    }
+
+    private mixed $fn;
+    /** @var array<mixed> */
+    private array $deps;
+
+    /**
+     * @param array<mixed> $deps
+     */
+    public function __construct(
+        Renderer $renderer,
+        Node $node,
+        Closure $fn,
+        array $deps,
+    ) {
+        parent::__construct(
+            renderer: $renderer,
+            node: $node,
+        );
+
+        $this->fn = $fn;
+        $this->deps = $deps;
+    }
+
+    public function initialRender(mixed ...$args): mixed
+    {
+        return $this->fn;
+    }
+
+    public function rerender(mixed ...$args): mixed
+    {
+        /** @var Closure $fn */
+        $fn = $args[0];
+        /** @var array<mixed> $deps */
+        $deps = $args[1];
+
+        if (!$this->renderer->valuesAreEqual($this->deps, $deps)) {
+            $this->fn = $fn;
+        }
+
+        return $this->fn;
+    }
+}
